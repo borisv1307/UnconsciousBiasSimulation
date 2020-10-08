c07eff00d0adff5e86a2e40200447f8f03c86a22
Shivani Aggarwal <sa3664@drexel.edu>
2020-10-08 00:47:18 -0400

    Backend configuration update

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,15 @@ from flask_cors import CORS
 from flask_restful import Api
 from flask import Flask
 from resources.home import Home
-from resources.user import UserRegistration, UserLogin
-from resources.profile import CreateProfile, ViewProfile
+from resources.profile import CreateProfile
 from config import create_app
 
 
 app = create_app()
 api = Api(app)
 api.add_resource(Home, '/')
-api.add_resource(UserRegistration, '/register')
-api.add_resource(UserLogin, '/login')
-api.add_resource(CreateProfile, '/createProfile/<usernane>')
-api.add_resource(ViewProfile, '/viewProfile')
+api.add_resource(CreateProfile, '/createProfile/<email>')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
