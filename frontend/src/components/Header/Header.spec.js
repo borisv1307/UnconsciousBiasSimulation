fdea9e6e8ad5c977250addbda37ae334bc3ff511
Shivani Aggarwal <sa3664@drexel.edu>
2020-10-14 14:08:47 -0400

    Header component and TDD

---
+++ frontend/src/components/Header/Header.spec.js
@@ -0,0 +1,31 @@
+import Header from "./Header";
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// describe("Header Component", () => {
+//   test("renders", () => {
+//     const links = [
+//       { link: "http://localhost:3000/home", text: "Home" },
+//       { link: "", text: "Create Profile" },
+//       { link: "", text: "View Profile" },
+//     ];
+//     const wrapper = shallow(<Header links={links} />);
+//     expect(wrapper.exists()).toBe(true);
+//   });
+// });
+
+describe("Header", () => {
+  it("Header should exist", () => {
+    const links = [
+      { link: "http://localhost:3000/home", text: "Home" },
+      { link: "http://localhost:3000/createProfile", text: "Create Profile" },
+      { link: "http://localhost:3000/viewProfile", text: "View Profile" },
+    ];
+    const wrapper = shallow(<Header links={links} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
