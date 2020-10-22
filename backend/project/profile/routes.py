9a1c2019f1d8d47813bb094fdb2cba28c54c581d
Jijo George <57690123+Jijogeorge69@users.noreply.github.com>
2020-10-21 22:36:08 -0400

    Rollback

--- backend/project/profile/routes.py
+++ backend/project/profile/routes.py
@@ -25,8 +25,7 @@ def profile_validation(f):
         try:
             profile_data = request.get_json()
             get_email = profile_data['email']
-        except ValueError as ev:
-            print('Error',ev)
+        except:
             return {'code': 4, 'error': 'Missing request body'}, 403
 
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
@@ -122,7 +121,6 @@ def get_user_profiles():
             output = {'code': 2, "error": "User not found"}
         else:
             output = {"count": len(output), "results": output}
-    except ValueError as ev:
-        print('Error',ev)
+    except:
         output = {'code': 2, "error": "Error fetching details from DB"}
     return output
