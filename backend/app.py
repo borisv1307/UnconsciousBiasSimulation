7ef901464156f8e94053a91f566efed46519a293
Jijo George <57690123+Jijogeorge69@users.noreply.github.com>
2020-10-27 02:49:47 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
+#pylint: disable = line-too-long, too-many-lines, no-name-in-module, import-error, multiple-imports, pointless-string-statement, wrong-import-order
 from flask_cors import CORS
 from flask_restful import Api
 from flask import Flask
 from project import create_app
 
-app = create_app()
+app = create_app('dev')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['JSON_SORT_KEYS'] = False
 
