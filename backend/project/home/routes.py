6734ebae1e2d3cdc297ee5868f5b89d65a4284a9
Jijo George <57690123+Jijogeorge69@users.noreply.github.com>
2020-10-27 02:45:59 -0400

    Update routes.py

--- backend/project/home/routes.py
+++ backend/project/home/routes.py
@@ -1,5 +1,5 @@
+#pylint: disable = missing-function-docstring ,missing-final-newline, missing-module-docstring, missing-function-docstring, line-too-long, too-many-lines, no-name-in-module, import-error, multiple-imports, pointless-string-statement, wrong-import-order
 from . import home_blueprint
-
 ################
 #### routes ####
 ################
