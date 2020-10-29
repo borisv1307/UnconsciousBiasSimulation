ab0f9cf0caa850b421c0a3434eff1902672d0fa7
Jijo George <57690123+Jijogeorge69@users.noreply.github.com>
2020-10-28 23:42:50 -0400

    Update test_profile.py

--- backend/tests/functional/test_profile.py
+++ backend/tests/functional/test_profile.py
@@ -153,7 +153,7 @@ class TestSomething:
         """
 
         data = {
-        "email":"max@gmail.com",
+        "email":"jasonmax@gmail.com",
         "profileName":profilename,
         "profileImg":"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
         "first_name": "Test",
