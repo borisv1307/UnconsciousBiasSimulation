3b585d65e5f12e66001caacf2a4f6f59565fce4f
Shivani Aggarwal <sa3664@drexel.edu>
2020-11-04 18:30:49 -0500

    Login component and TDD

--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,106 +1,109 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
 
 class Login extends Component {
-      state = {
-        email:"",
-        password:"",
-      };  
-      updateField = (stateKey) => (e) => {
-        this.setState({ [stateKey]: e.target.value });
-      };
-    handleLogin = async (e) => {
-          console.log('Submit Click');
-        //e.preventDefault();
-        const redirectToHome = () =>{ 
-            window.location.href='./createProfile';
+  state = {
+    email: "",
+    password: "",
+  };
+  updateField = (stateKey) => (e) => {
+    this.setState({ [stateKey]: e.target.value });
+  };
+  handleLogin = async (e) => {
+    //e.preventDefault();
+    const redirectToHome = () => {
+      window.location.href = "./home";
+    };
+
+    const data = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    fetch("http://localhost:5000/api/v1/login/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.user_id) {
+          redirectToHome();
+        } else {
+          alert(res.error);
         }
-        
-        const data = {
-          email: this.state.email,
-          password: this.state.password,
-        };
-    
-        console.log(JSON.stringify(data));
-        
-        fetch("http://localhost:5000//api/v1/login/", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify(data),
-        })
-          .then((res) => res.json())
-          .then((res)=>{
-              if(res.user_id){
-                redirectToHome();
-              }
-              else{
-                alert(res.error);
-              }
-          });  
-        
-          this.setState({ 
-            email: "",
-            password:"",
-        }); 
-      };
-    render(){
-        return(
-            <div className="justify-content-end header" >
-                <br/>
-                <h4 className="text-center">Unconscious Bias Simulation</h4>
-                <Container className="containbody justify-content-center">
-                <nav class="navbar navbar-dark">
-                    <div className="row col-12 d-flex justify-content-left text-black">
-                    <span className="h3">Login</span>
-                    </div>
-                </nav><br/>    
-                <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+      });
+
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+  render() {
+    return (
+      <div className="justify-content-end header">
+        {/* <br /> */}
+        <h4>Unconscious Bias Simulation</h4>
+        <br />
+        <br />
+        <Container className="containbody justify-content-center">
+          <nav class="navbar navbar-dark">
+            <div className="row col-12 d-flex justify-content-left text-black">
+              <span className="h3">Login</span>
+            </div>
+          </nav>
+          <br />
+          <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
             <form id="Form">
-                <div className="form-group text-left">
+              <div className="form-group text-left">
                 <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" 
-                       className="form-control" 
-                       onChange={this.updateField("email")}
-                       id="email" 
-                       aria-describedby="emailHelp" 
-                       placeholder="Enter email" 
-                       
-                       
+                <input
+                  type="email"
+                  className="form-control"
+                  onChange={this.updateField("email")}
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
                 />
-                
-                </div>
-                <div className="form-group text-left">
+              </div>
+              <div className="form-group text-left">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" 
-                       className="form-control" 
-                       onChange={this.updateField("password")}
-                       id="password" 
-                       placeholder="Password"
-                       
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={this.updateField("password")}
+                  id="password"
+                  placeholder="Password"
                 />
-                </div>
-                <div className="form-check">
-                </div>
-                <Button 
-                    id="submit" 
-                    className="buttonnprimary"
-                    onClick={this.handleLogin}
-                >Submit</Button>
+              </div>
+              <div className="form-check"></div>
+              <Button
+                id="submit"
+                className="buttonnprimary"
+                onClick={this.handleLogin}
+              >
+                Submit
+              </Button>
             </form>
             <div className="registerMessage">
-                <span>Dont have an account? </span>
-                <Button variant="link" onClick={event =>  window.location.href='./register'}>Register</Button> 
+              <span>Dont have an account? </span>
+              <Button
+                id="register"
+                variant="link"
+                onClick={(event) => (window.location.href = "./register")}
+              >
+                Register
+              </Button>
             </div>
-        </div>
-                </Container>
-            </div>
-        );
-    }
-
-
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 export default Login;
--- frontend/src/components/Login/login.spec.js
+++ frontend/src/components/Login/login.spec.js
@@ -7,18 +7,54 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 const simulateChangeOnInput = (wrapper, inputSelector, newValue) => {
-    const input = wrapper.find(inputSelector);
-    input.simulate("change", {
-      target: { value: newValue },
-    });
-    return wrapper.find(inputSelector);
-  };
+  const input = wrapper.find(inputSelector);
+  input.simulate("change", {
+    target: { value: newValue },
+  });
+  return wrapper.find(inputSelector);
+};
 
 describe("Login", () => {
-    let wrapper;
+  let wrapper;
   beforeEach(() => (wrapper = shallow(<Login />)));
+
+  it("Login should exist", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("Login should include h4", () => {
+    const container = wrapper.find("h4");
+    expect(container.length).toEqual(1);
+  });
+
+  it("Login should include container", () => {
+    const container = wrapper.find("Container");
+    expect(container.length).toEqual(1);
+  });
+
+  it("should have a form", () => {
+    expect(wrapper.find("form").length).toEqual(1);
+  });
+
   it("should include inputs for login", () => {
     expect(wrapper.find("#email").length).toEqual(1);
     expect(wrapper.find("#password").length).toEqual(1);
+  });
+
+  it("should include buttons for submit and register", () => {
+    expect(wrapper.find("Button#submit").length).toEqual(1);
+    expect(wrapper.find("Button#register").length).toEqual(1);
+  });
+
+  it("render submit button with custom text", () => {
+    const button = wrapper.find("Button#submit");
+    expect(button).toHaveLength(1);
+    expect(button.text()).toEqual("Submit");
+  });
+
+  it("render register button with custom text", () => {
+    const button = wrapper.find("Button#register");
+    expect(button).toHaveLength(1);
+    expect(button.text()).toEqual("Register");
   });
 });
