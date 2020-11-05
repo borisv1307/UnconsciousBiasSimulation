5da2f4e1b14bcf947dc1dc06f8fbbd4bf1e76861
Shivani Aggarwal <sa3664@drexel.edu>
2020-11-04 22:10:23 -0500

    Home Fix

--- frontend/src/components/Home/Home.spec.js
+++ frontend/src/components/Home/Home.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Home from "./home";
+import Home from "./Home";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
