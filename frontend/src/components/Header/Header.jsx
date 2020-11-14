a60a182eb1a100280168605182beeff6dc834f29
Shivani Aggarwal <sa3664@drexel.edu>
2020-11-15 00:11:51 +0530

    Header changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import ls from 'local-storage'
+import ls from "local-storage";
 
 class Header extends Component {
-
   render() {
-    let user= ls.get('userid');
-    let name= ls.get('name');
+    let user = ls.get("userid");
+    let name = ls.get("name");
     return (
-      
       <>
-        
-        <Navbar className="header">
-          <Navbar.Brand href="/home">{"Unconscious Bias Simulation: " + name}</Navbar.Brand>
+        <style type="text/css">
+          {`
+      .nav-style {
+        font-size: x-large;
+      }
+      .nav-style-title {
+        font-size: xx-large;
+      }
+          `}
+        </style>
+        <Navbar className=" header font-weight-bold">
+          <Navbar.Brand className="nav-style-title " href="/home">
+            {"Unconscious Bias Simulation: " + name}
+          </Navbar.Brand>
           <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
+          <Navbar.Collapse className="justify-content-end nav-style">
             {/* <Nav.Link href="/home">Home</Nav.Link> */}
-            <Nav.Link href={'/createProfile?userId=' + user}>Create Application</Nav.Link>
-            <Nav.Link href={'/viewProfile?userId=' + user}>View Application</Nav.Link>
+            <Nav.Link href={"/createProfile?userId=" + user}>
+              Create Presence
+            </Nav.Link>
+            <Nav.Link href={"/viewProfile?userId=" + user}>
+              View Application
+            </Nav.Link>
           </Navbar.Collapse>
         </Navbar>
         <br />
