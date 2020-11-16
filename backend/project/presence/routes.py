c87d1acc1b4cdc02ac8370adb3a7f6835ab13562
vighnajeet <vighnajeet@gmail.com>
2020-11-16 18:08:39 -0500

    fixes for pylint

--- backend/project/presence/routes.py
+++ backend/project/presence/routes.py
@@ -63,39 +63,6 @@ def insert_data(profile_information):
     return "ERROR"
 
 
-@presence_blueprint.route('/api/v1/getPresence/<user_id>/', methods=['GET'])
-def get_user_presence(user_id):
-
-    # Convert id to integer
-    try:
-        user_id = int(user_id)
-    except:
-        return {'code': 5, 'error': "id must be numerical"}, 403
-
-    # Get request
-    presence = mongo.db.presence
-    user_presence = presence.find_one({'user_id': user_id})
-    if get_user_presence:
-        output = {
-            'user_id': int(user_presence['user_id']),
-            'profile_id': user_presence['profile_id'],
-            'profile_name': user_presence['profileName'],
-            'profile_image': user_presence['profileImg'],
-            'first_name': user_presence['first_name'],
-            'last_name': user_presence['last_name'],
-            'about_me': user_presence['aboutMe'],
-            'position': user_presence['position'],
-            'education': user_presence['education'],
-            'experience': user_presence['experience'],
-            'status': user_presence['status'],
-            'reviewed_on': user_presence['reviewedOn'],
-            'reviewed_by': user_presence['reviewedBy']
-        }
-    else:
-        output = {'code': 5, "error": "Presence not found"}, 403
-        return output
-
-
 @presence_blueprint.route('/api/v1/getAllPresence/', methods=['GET'])
 def get_all_presence():
     if request.method == 'GET':
