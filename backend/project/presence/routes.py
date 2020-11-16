53635560343d77ef7d9b3e340cc21ae95075c68a
vighnajeet <vighnajeet@gmail.com>
2020-11-16 17:32:14 -0500

    API to display presence for HR

--- backend/project/presence/routes.py
+++ backend/project/presence/routes.py
@@ -10,6 +10,7 @@ from . import presence_blueprint
 
 # ALL FUTURE DATA VALIDATION
 
+
 @presence_blueprint.route('/api/v1/addPresence/', methods=['POST'])
 def add_presence_to_pool():
     date_joined = datetime.utcnow()
@@ -28,14 +29,15 @@ def add_presence_to_pool():
             "profileImg": profile_data['profileImg'],
             "first_name": profile_data['first_name'],
             "last_name": profile_data['last_name'],
-            "status":"submitted",
+            "status": "submitted",
             "addedOn": date_joined,
             "reviewedOn": "",
             "reviewedBy": ""
-            }
+        }
     else:
         result = {'code': 2, "error": "User account does not exist"}, 403
     return result
+
 
 def insert_data(profile_information):
     date_joined = datetime.utcnow()
@@ -51,7 +53,7 @@ def insert_data(profile_information):
         "aboutMe":  profile_information['aboutMe'],
         "education": profile_information['education'],
         "experience": profile_information['experience'],
-        "status":"submitted",
+        "status": "submitted",
         "addedOn": date_joined,
         "reviewedOn": "",
         "reviewedBy": ""
@@ -59,3 +61,66 @@ def insert_data(profile_information):
     if create_presence:
         return profile_information
     return "ERROR"
+
+
+@presence_blueprint.route('/api/v1/getPresence/<user_id>/', methods=['GET'])
+def get_user_presence(user_id):
+
+    # Convert id to integer
+    try:
+        user_id = int(user_id)
+    except:
+        return {'code': 5, 'error': "id must be numerical"}, 403
+
+    # Get request
+    presence = mongo.db.presence
+    get_user_presence = presence.find_one({'user_id': user_id})
+    if get_user_presence:
+        output = {
+            'user_id': int(get_user_presence['user_id']),
+            'profile_id': get_user_presence['profile_id'],
+            'profile_name': get_user_presence['profileName'],
+            'profile_image': get_user_presence['profileImg'],
+            'first_name': get_user_presence['first_name'],
+            'last_name': get_user_presence['last_name'],
+            'about_me': get_user_presence['aboutMe'],
+            'position': get_user_presence['position'],
+            'education': get_user_presence['education'],
+            'experience': get_user_presence['experience'],
+            'status': get_user_presence['status'],
+            'reviewed_on': get_user_presence['reviewedOn'],
+            'reviewed_by': get_user_presence['reviewedBy']
+        }
+    else:
+        output = {'code': 5, "error": "Presence not found"}, 403
+        return output
+
+
+@presence_blueprint.route('/api/v1/getAllPresence/', methods=['GET'])
+def get_all_presence():
+    if request.method == 'GET':
+        all_presence = mongo.db.presence
+
+        output = []
+        try:
+            for presence in all_presence.find():
+
+                output.append({
+                    'user_id': int(presence['user_id']),
+                    'profile_id': presence['profile_id'],
+                    'profile_name': presence['profileName'],
+                    'profile_image': presence['profileImg'],
+                    'first_name': presence['first_name'],
+                    'last_name': presence['last_name'],
+                    'about_me': presence['aboutMe'],
+                    'position': presence['position'],
+                    'education': presence['education'],
+                    'experience': presence['experience'],
+                    'status': presence['status'],
+                    'reviewed_on': presence['reviewedOn'],
+                    'reviewed_by': presence['reviewedBy']
+                })
+
+            return {'count': len(output), 'results': output}
+        except:
+            return {'code': 4, 'error': "No presence found"}, 403
--- backend/tests/functional/test_presence.py
+++ backend/tests/functional/test_presence.py
@@ -3,20 +3,20 @@ This file (test_profile.py) contains the functional tests which
 test create profile and view profile.
 
 """
-#pylint: disable = line-too-long, too-many-lines, no-name-in-module, import-error, multiple-imports, pointless-string-statement, wrong-import-order
+# pylint: disable = line-too-long, too-many-lines, no-name-in-module, import-error, multiple-imports, pointless-string-statement, wrong-import-order
+from random import randint
+from bson.objectid import ObjectId
+from project import mongo
+from project import create_app
 import pytest
 import os
 import sys
 from flask import jsonify, request, json
 from datetime import datetime
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-PARENT_ROOT=os.path.abspath(os.path.join(SITE_ROOT, os.pardir))
-GRANDPAPA_ROOT=os.path.abspath(os.path.join(PARENT_ROOT, os.pardir))
-sys.path.insert(0,GRANDPAPA_ROOT)
-from project import create_app
-from project import mongo
-from bson.objectid import ObjectId
-from random import randint
+PARENT_ROOT = os.path.abspath(os.path.join(SITE_ROOT, os.pardir))
+GRANDPAPA_ROOT = os.path.abspath(os.path.join(PARENT_ROOT, os.pardir))
+sys.path.insert(0, GRANDPAPA_ROOT)
 
 profilename = "Profile B"
 
@@ -32,45 +32,57 @@ def test_client():
 
 class TestPool:
 
-    def test_for_adding_presence_for_pool(self,test_client):
+    def test_for_adding_presence_for_pool(self, test_client):
         """
         GIVEN a Flask application configured for testing
         WHEN the '/addPresence' page is requested (POST)
         THEN check that request has email address
         """
         data = {
-        "profileName":profilename,
-        "user_id": 1,
-        "profile_id":1,
-        "profileImg":"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-        "first_name": "Test",
-        "last_name": "User",
-        "position": "Developer",
-        "aboutMe": "Hello World",
-        "education": [
-            {
-            "school": "Drexel",
-            "degree": "MA",
-            "major": "SE",
-            "eduStartDate": "0001-01",
-            "eduEndDate": "0001-01",
-            "gpa": "3"
-            }
-        ],
-        "experience": [
-            {
-            "title": "Developer",
-            "company": "ABC",
-            "location": "PH",
-            "expStartDate": "0001-01",
-            "expEndDate": "0001-01"
-            }
-        ],
-        "status": "submitted",
-        "reviewedBy": "",
-        "addedOn": datetime.utcnow(),
-        "reviewedOn": ""
+            "profileName": profilename,
+            "user_id": 1,
+            "profile_id": 1,
+            "profileImg": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+            "first_name": "Test",
+            "last_name": "User",
+            "position": "Developer",
+            "aboutMe": "Hello World",
+            "education": [
+                {
+                    "school": "Drexel",
+                    "degree": "MA",
+                    "major": "SE",
+                    "eduStartDate": "0001-01",
+                    "eduEndDate": "0001-01",
+                    "gpa": "3"
+                }
+            ],
+            "experience": [
+                {
+                    "title": "Developer",
+                    "company": "ABC",
+                    "location": "PH",
+                    "expStartDate": "0001-01",
+                    "expEndDate": "0001-01"
+                }
+            ],
+            "status": "submitted",
+            "reviewedBy": "",
+            "addedOn": datetime.utcnow(),
+            "reviewedOn": ""
         }
-        response = test_client.post('/api/v1/addPresence/', data=json.dumps(data),headers={'Content-Type': 'application/json'})
+        response = test_client.post(
+            '/api/v1/addPresence/', data=json.dumps(data), headers={'Content-Type': 'application/json'})
         assert response.status_code == 200
         assert response != 'null'
+
+    def test_get_all_presence(self, test_client):
+        """
+        GIVEN a Flask application configured for testing
+        WHEN the '/api/v1/getAllPresence/' page is requested (POST)
+        THEN check that the response is valid
+        """
+        response = test_client.get(
+            '/api/v1/getAllPresence/', headers={'Content-Type': 'application/json'})
+        assert response.status_code == 200
+        assert response.data != 'null'
