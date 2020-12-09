90ca751841271dabf6b0ea0db941af75cf7a575a
Shivani Aggarwal <sa3664@drexel.edu>
2020-12-09 23:07:59 +0530

    HomeHR imp

--- frontend/src/components/Home/HomeHR.jsx
+++ frontend/src/components/Home/HomeHR.jsx
@@ -17,16 +17,6 @@ class HomeHR extends Component {
         datasets: [
           {
             data: [300, 50, 100, 40, 120],
-            // backgroundColor: ['rgba(75, 192, 192, 0.2)',
-            //   'rgba(255, 99, 132, 0.2)', 'rgba(255, 205, 86, 0.2)',
-            //   'rgba(201, 203, 207, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-            // hoverBackgroundColor: [
-            //   'rgba(75, 192, 192, 0.2)',
-            //   'rgba(255, 99, 132, 0.2)',
-            //   'rgba(255, 205, 86, 0.2)',
-            //   'rgba(201, 203, 207, 0.2)',
-            //   'rgba(153, 102, 255, 0.2)'
-            // ]
             backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
             hoverBackgroundColor: [
               "#FF5A5E",
