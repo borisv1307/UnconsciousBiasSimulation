48912b286fd59002af3a78231de45c1c7040f9a9
Shivani Aggarwal <sa3664@drexel.edu>
2020-12-10 03:23:37 +0530

    horizontal Bar graph state change

--- frontend/src/components/graphs/horizontalBarGraph.jsx
+++ frontend/src/components/graphs/horizontalBarGraph.jsx
@@ -3,26 +3,29 @@ import { Container, Row, Col } from "react-bootstrap";
 import { HorizontalBar } from 'react-chartjs-2';
 
 class HorizontalBarGraph extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            dataHorizontal: this.props.inputData
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataHorizontal: this.props.inputData,
+      barChartOptions: this.props.barChartOptions ? this.props.barChartOptions : "{ responsive: true }",
+      height: this.props.height ? this.props.height : 150
     }
+  }
 
   render() {
     return (
       <Container>
         <br />
         <Row>
-            <Col></Col>
-            <Col xs={10}>
+          <Col></Col>
+          <Col xs={10}>
             <HorizontalBar
-                data={this.state.dataHorizontal}
-                options={{ responsive: true }}
+              data={this.state.dataHorizontal}
+              options={this.state.barChartOptions}
+              height={this.state.height}
             />
-            </Col>
-            <Col></Col>
+          </Col>
+          <Col></Col>
         </Row>
         <br />
       </Container>
